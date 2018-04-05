c044ace1687ba5d9cda3cc7d92a71e0f8e23462c
bradyjoestar <bradyjoestar@gmail.com>
2018-04-05 17:38:07 +0800

    Update kubedam_start.sh

--- master/kubedam_start.sh
+++ master/kubedam_start.sh
@@ -4,4 +4,5 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/canal.yaml
