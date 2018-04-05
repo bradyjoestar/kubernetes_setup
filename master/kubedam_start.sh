188d7ad6c8bd34dd0912e6a8cbcf5c5c82500fb0
bradyjoestar <bradyjoestar@gmail.com>
2018-04-05 17:11:24 +0800

    Update kubedam_start.sh

--- master/kubedam_start.sh
+++ master/kubedam_start.sh
@@ -1,7 +1,7 @@
 go get github.com/kubernetes-incubator/cri-tools/cmd/crictl
 
-kubeadm init
+kubeadm init --pod-network-cidr=10.244.0.0/16
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubectl apply --pod-network-cidr=10.244.0.0/16 -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
