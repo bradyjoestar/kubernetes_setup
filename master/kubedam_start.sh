53e3c66a61912f91469c5c789faab4f3b316d829
bradyjoestar <bradyjoestar@gmail.com>
2018-04-05 19:22:49 +0800

    Update kubedam_start.sh

--- master/kubedam_start.sh
+++ master/kubedam_start.sh
@@ -2,7 +2,3 @@ go get github.com/kubernetes-incubator/cri-tools/cmd/crictl
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
-
-
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/canal.yaml
