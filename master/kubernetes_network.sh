f74118b2e26fae13534dae11a9290983c604f0da
bradyjoestar <bradyjoestar@gmail.com>
2018-04-05 19:23:27 +0800

    Create kubernetes_network.sh

---
+++ master/kubernetes_network.sh
@@ -0,0 +1,4 @@
+
+
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/canal/master/k8s-install/1.7/canal.yaml
